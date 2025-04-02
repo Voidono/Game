9f955b576a7bd0dd2a5f31c74516566b54f92b8c
thanhfpate <Tangtienthanh208@gmail.com>
2025-04-03 00:46:00 +0700

    init

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 @export var gravity: float = 500.0
+@onready var anim = get_node("AnimatedSprite2D")
+@onready var container = get_node("container")
 
 func _physics_process(delta):
 	# Áp dụng trọng lực
@@ -15,12 +17,20 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * speed
 		# Quay mặt theo hướng di chuyển
-		if direction > 0:
-			$An1.flip_h = false
-		else:
-			$An1.flip_h = true
+	if direction == 1 or direction == -1:
+		velocity.x = direction * speed
+		container.scale.x = direction
 	else:
-		velocity.x = 0
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+		
+	# Anim nhân vật
+	if direction == 1 or direction == -1:
+		anim.play("running")
+	else:
+		anim.play("stand")
+		
+		
 	
 	# Áp dụng chuyển động
 	move_and_slide()
