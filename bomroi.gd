55aa5378c60c8728b81bd36f219212d0e2ead156
thanhfpate <Tangtienthanh208@gmail.com>
2025-04-25 15:37:37 +0700

    init

---
+++ bomroi.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(_body:Node2D):
+	entered = true
+
+func _process(_delta):
+	if entered == true:
+		get_tree().change_scene_to_file("res://Minigame-1/game_ne_bomb.tscn")
+ 
+pass
